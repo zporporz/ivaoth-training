47c7f5e46d4d0d3a86ea8e5bd3e86929528f86ee
zporporz <ecgkasit@gmail.com>
2026-05-14 17:40:15 +0700

    Initial Next.js setup

---
+++ ivaoth-training/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        IVAO Thailand Training
+      </h1>
+    </main>
+  );
+}
