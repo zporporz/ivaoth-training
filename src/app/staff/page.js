6d5c01ba28f0cfddee96311837f7463652b9f649
zporporz <ecgkasit@gmail.com>
2026-06-05 12:32:37 +0700

    Render staff notice badge layer

--- src/app/staff/page.js
+++ src/app/staff/page.js
@@ -1,12 +1,14 @@
 "use client";
 
 import ProtectedStaffPage from "../../components/ProtectedStaffPage";
+import StaffNoticeBadgeLayer from "../../components/StaffNoticeBadgeLayer";
 import OriginalStaffPage from "./staff-content";
 
 export default function StaffPage() {
   return (
     <ProtectedStaffPage>
       <OriginalStaffPage />
+      <StaffNoticeBadgeLayer />
     </ProtectedStaffPage>
   );
 }
